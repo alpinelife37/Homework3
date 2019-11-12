8b37843df4a4bb1e657013196696cad0818f467d
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2019-11-12 13:39:07 -0800

    Update removing unwanted , in upper case array

--- code.js
+++ code.js
@@ -9,7 +9,7 @@ var capVal = confirm("Click OK if you want to use capital letters?");
 var symVal = ["~,","!","@","#","$","%","^","&","*","(",")","{","}",";",":","<",">","?"];
 var numVal=["1","2","3","4","5","6","7","8","9","0"];
 var lowVal = [   "a","b","c","d","e","f","g","h","i","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y,","z"];
-var capVal = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U,","V","W","X","Y","Z"];
+var capVal = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 var password = [];  
 var display = document.getElementById('display');
