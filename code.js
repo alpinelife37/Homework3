e8aa4093da31a93613b3d5bc605a80e9d510aaf5
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2019-11-12 19:59:00 -0800

    Removed non used code in the JS file

--- code.js
+++ code.js
@@ -1,171 +1,130 @@
+function generate() {
+  var charNums = prompt("How many characters do you desire?");
+  var symConfirm = confirm("Click OK if you want to use special characters?");
+  var numConfirm = confirm("Click OK if you want to use numbers?");
+  var lowConfirm = confirm("Click OK if you want to use lowercase letters?");
+  var capConfirm = confirm("Click OK if you want to use capital letters?");
+  var symVal = [
+    "~,",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "{",
+    "}",
+    ";",
+    ":",
+    "<",
+    ">",
+    "?"
+  ];
+  var numVal = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  var lowVal = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y,",
+    "z"
+  ];
+  var capVal = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+  ];
 
-function generate () {
-
-var charNums=prompt("How many characters do you desire?");
-var symConfirm= confirm("Click OK if you want to use special characters?");
-var numConfirm= confirm("Click OK if you want to use numbers?");
-var lowConfirm = confirm("Click OK if you want to use lowercase letters?");
-var capConfirm = confirm("Click OK if you want to use capital letters?");
-var symVal = ["~,","!","@","#","$","%","^","&","*","(",")","{","}",";",":","<",">","?"];
-var numVal=["1","2","3","4","5","6","7","8","9","0"];
-var lowVal = [   "a","b","c","d","e","f","g","h","i","j","k","l","m","n","p","q","r","s","t","u","v","w","x","y,","z"];
-var capVal = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-
-var password = [];  
-var display = document.getElementById('display');
+  var password = [];
+  var display = document.getElementById("display");
 
   console.log(charNums);
-for (var i = 0; i<charNums; i++  ) {
-  if (symConfirm) {
-    password.push(symVal[Math.floor(Math.random() * symVal.length)]);
-    i = i + 1;
-     }
-  if (i === charNums) { break; }
- console.log(i);
-  if (capConfirm) {
-    password.push(capVal[Math.floor(Math.random() * capVal.length)]);
-    i = i + 1;
-  }
-  if (i === charNums) {break;}
-
-  if (numConfirm){
-   password.push(numVal[Math.floor(Math.random() * numVal.length)]);
-   i = i + 1;
-  }
-  if (i === charNums) { break; }
-
-  if(lowConfirm){
-    password.push(lowVal[Math.floor(Math.random() * lowVal.length)]);
-    i = i + 1;
+  for (var i = 0; i < charNums; i++) {
+    if (symConfirm) {
+      password.push(symVal[Math.floor(Math.random() * symVal.length)]);
+      i = i + 1;
     }
-  if (i === charNums ) { break; }
+    if (i === charNums) {
+      break;
+    }
+    console.log(i);
+    if (capConfirm) {
+      password.push(capVal[Math.floor(Math.random() * capVal.length)]);
+      i = i + 1;
+    }
+    if (i === charNums) {
+      break;
+    }
 
-};
-// for (var i = 0; i<inputVal; i++  ) {
-// };
-// for (var i = 0; i<inputVal; i++  ) {
-// };
-// for (var i = 0; i<inputVal; i++  ) {
-   
-// };
+    if (numConfirm) {
+      password.push(numVal[Math.floor(Math.random() * numVal.length)]);
+      i = i + 1;
+    }
+    if (i === charNums) {
+      break;
+    }
 
-password = password.join("");
-display.value = password;
-
-};
-
-
-
-// var symRandom = symVal[Math.floor(Math.random() * symVal.length)];
-// console.log("symRandom", symRandom);
-// var numRandom = numVal[Math.floor(Math.random() * numVal.length)];
-// console.log(numRandom);
-
-// console.log(lowRandom);
-// var capRandom = capVal[Math.floor(Math.random() * capVal.length)];
-// console.log(capRandom);     
-
-
-
-
-
-
-
-
-
-
-//function generatePassword(){
-  // user says 9 
- 
-//}};
-
-      
- //for (var i = 0; i < valInput; i++) {
-
-   
-//   if (sconf === true){
-//         passwordSym = passwordSym + symRandom.charAt(Math.floor(Math.random()* symRandom.length));
-//         document.getElementById("password").innerHTML = passwordSym;
-//     } else if (nconf === true) {
-//         passwordNum = passwordNum + numRandom.charAt(Math.floor(Math.random()* numRandom.length));
-//         document.getElementById("password").innerHTML = passwordNum;
-//     } else if (lconf === true) {
-//         passwordLow = passwordLow + lowRandom.charAt(Math.floor(Math.random()* lowRandom.length));
-//         document.getElementById("password").innerHTML = passwordLow;
-//     }else {
-//         passwordCap = passwordCap + capRandom.charAt(Math.floor(Math.random()* capRandom.length));
-//         document.getElementById("password").innerHTML = passwordCap
-// };
-
-  function copyPassword() {
-    document.getElementById("display").select();
-      document.execCommand("Copy");
-      alert("Password copied to clipboard!");
+    if (lowConfirm) {
+      password.push(lowVal[Math.floor(Math.random() * lowVal.length)]);
+      i = i + 1;
+    }
+    if (i === charNums) {
+      break;
+    }
   }
 
+  password = password.join("");
+  display.value = password;
+}
 
-
-//    function passwordNum(){
-//     if (sconf === true){
-//         console.log(symRandom);
-//         passwordSyb = passwordSyb + symRandom.charAt(Math.floor(Math.random()* symRandom.length));
-//         document.getElementById("password").innerHTML = passwordSyb;
-//         console.log passwordSyb);
-//     } else if (nconf === true) {
-//         console.log(numRandom);
-//         passwordNum = passwordNum + numRandom.charAt(Math.floor(Math.random()* numRandom.length));
-//         document.getElementById("password").innerHTML = passwordNum;
-//         console.log(passwordNum);
-//     } else if (lconf === true) {
-//         console.log(lowRandom);
-//         passwordLow = passwordLow + lowRandom.charAt(Math.floor(Math.random()* lowRandom.length));
-//         document.getElementById("password").innerHTML = passwordLow;
-//     }else {
-//         console.log(capRandom);
-//         passwordCap = passwordCap + capRandom.charAt(Math.floor(Math.random()* capRandom.length));
-//         document.getElementById("password").innerHTML = passwordCap
-// }
-//    };
-// function passwordLow(){
-//   if (sconf === true){
-//       console.log(symRandom);
-//       passwordSyb = passwordSyb + symRandom.charAt(Math.floor(Math.random()* symRandom.length));
-//       document.getElementById("password").innerHTML = passwordSyb;
-//       console.log passwordSyb);
-//   } else if (nconf === true) {
-//       console.log(numRandom);
-//       passwordNum = passwordNum + numRandom.charAt(Math.floor(Math.random()* numRandom.length));
-//       document.getElementById("password").innerHTML = passwordNum;
-//       console.log(passwordNum);
-//   } else if (lconf === true) {
-//       console.log(lowRandom);
-//       passwordLow = passwordLow + lowRandom.charAt(Math.floor(Math.random()* lowRandom.length));
-//       document.getElementById("password").innerHTML = passwordLow;
-//   }else {
-//       console.log(capRandom);
-//       passwordCap = passwordCap + capRandom.charAt(Math.floor(Math.random()* capRandom.length));
-//       document.getElementById("password").innerHTML = passwordCap
-// }
-// };
-// function passwordCap(){
-//   if (sconf === true){
-//       console.log(symRandom);
-//       passwordSyb = passwordSyb + symRandom.charAt(Math.floor(Math.random()* symRandom.length));
-//       document.getElementById("password").innerHTML = passwordSyb;
-//       console.log passwordSyb);
-//   } else if (nconf === true) {
-//       console.log(numRandom);
-//       passwordNum = passwordNum + numRandom.charAt(Math.floor(Math.random()* numRandom.length));
-//       document.getElementById("password").innerHTML = passwordNum;
-//       console.log(passwordNum);
-//   } else if (lconf === true) {
-//       console.log(lowRandom);
-//       passwordLow = passwordLow + lowRandom.charAt(Math.floor(Math.random()* lowRandom.length));
-//       document.getElementById("password").innerHTML = passwordLow;
-//   }else {
-//       console.log(capRandom);
-//       passwordCap = passwordCap + capRandom.charAt(Math.floor(Math.random()* capRandom.length));
-//       document.getElementById("password").innerHTML = passwordCap
-// }
-//   };
-
+function copyPassword() {
+  document.getElementById("display").select();
+  document.execCommand("Copy");
+  alert("Password copied to clipboard!");
+}
